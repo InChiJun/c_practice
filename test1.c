ede4e3d8c8fe4be3fdf6cc2ad9dc4cdf79df08c0
Chijun <chijun@aqara.kr>
2023-11-29 16:48:31 +0900

    feat: add test1.c contents

--- test1.c
+++ test1.c
@@ -20,12 +20,20 @@ void solution1() {
     long long ll = -99999999999;
     unsigned long long ull = -999999;
 
+    printf("i = %d\n", i);
     printf("ui = %u\n", ui);
     printf("c = %c\n", c);
-    printf("uc = %d\n", uc);
+    printf("c = %d\n", c);
+    printf("uc = %u\n", uc);
     printf("uc = %c\n", uc);
-    printf("us = %d\n", us);
+    printf("f = %f\n", f);
+    printf("s = %d\n", s);
+    printf("s = %hd\n", s);
+    printf("us = %u\n", us);
     printf("us = %hu\n", us);
+    printf("l = %ld\n", l);
     printf("ul = %lu\n", ul);
+    printf("d = %lf\n", d);
+    printf("ll = %lld\n", ll);
     printf("ull = %llu\n", ull);
 }
